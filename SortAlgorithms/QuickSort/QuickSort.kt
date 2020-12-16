2a2cc8ccae4aae81a965e4834a92dc30e9ee7d8b
Michael Lennox Kent <lennoxkent@gmail.com>
2020-12-16 20:38:12 +0200

    almost working

--- SortAlgorithms/QuickSort/QuickSort.kt
+++ SortAlgorithms/QuickSort/QuickSort.kt
@@ -1,6 +1,29 @@
 class QuickSort{
   companion object {
-    fun sort(input: Array<Int>){
+    fun sort(input: Array<Int>, left: Int, right: Int){
+      if(left < right){
+        val pivot = splitPartition(input, left, right)
+        sort(input, left, pivot-1)
+        sort(input, pivot+1, right)
+      }
+    }
+    fun splitPartition(input: Array<Int>, left: Int, right: Int): Int{
+      var pivotValue = input[left]
+      var l: Int = left
+      var r: Int = right 
+      while(l<r){
+        while(input[l] <= pivotValue && l < input.size) l++ 
+        while(input[r] >= pivotValue && r > -1) r--
+        switch(input, l, r)
+      }
+      switch(input, r, l)
+      switch(input, left, r)
+      return r
+    }
+    fun switch(input: Array<Int>, a: Int, b: Int){
+      val temp: Int = input[b]
+      input[b] = input[a]
+      input[a] = temp
     }
   }
 }
@@ -14,8 +37,10 @@ fun printArray(input: Array<Int>){
 }
 
 fun main(){
-  val input: Array<Int> = arrayOf<Int>(1,3,4,9,0,6,2)
+  val input: Array<Int> = arrayOf<Int>(5,3,1,9,8,2,4,7)
+  println("Unsorted array")
   printArray(input)
-  println("Sorting array")
-
+  println("Sorted array")
+  QuickSort.sort(input, 0, input.size-1)
+  printArray(input)
 }
