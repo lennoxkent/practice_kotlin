04bb263ff479f85dad232ee3d86ee214bf8ad93e
Michael Lennox Kent <lennoxkent@gmail.com>
2021-01-27 22:03:13 +0200

    fix the test to have the quicksort expected results sorted

--- src/test/kotlin/sorting_algorithms/QuickSortTest.kt
+++ src/test/kotlin/sorting_algorithms/QuickSortTest.kt
@@ -1,31 +1,13 @@
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertArrayEquals
 
 class QuickSortTest {
-    companion object {
-        @BeforeAll
-        @JvmStatic
-        fun beforeAllTestCases() {
-            println("Runs once before all test cases.")
-        }
-
-        @AfterAll
-        @JvmStatic
-        fun afterAllTestCases() {
-            println("Runs once after all test cases.")
-        }
-    }
-
-    @Test
-    fun simpleTestCase() {
-        println("A simple test case.")
-
-        val input: Array<Int> = arrayOf<Int>(5,3,1,9,8,2,4,7)
-        //val input: Array<Int> = arrayOf<Int>(2,3,1,4)
-      //  QuickSort.sort(input, 0, input.size-1)
-      //  QuickSort.printArray(input)
-      // assertEquals(2, numberTwo)
-    }
+  @Test
+  fun simpleTestCase() {
+    val input: Array<Int> = arrayOf<Int>(5,3,1,9,8,2,4,7)
+    val expected: Array<Int> = arrayOf<Int>(1,2,3,4,5,7,8,9)
+    
+    QuickSort.sort(input, 0, input.size-1)
+    assertArrayEquals(expected, input)
+  }
 }
