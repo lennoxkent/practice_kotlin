64f2df60343b1f77fcef882cfb5dcff1678045d9
Michael Lennox Kent <lennoxkent@gmail.com>
2021-02-18 07:28:53 +0200

    Update quicksort and it's test

--- src/main/kotlin/sorting_algorithms/QuickSort.kt
+++ src/main/kotlin/sorting_algorithms/QuickSort.kt
@@ -1,56 +1,38 @@
+// Taken from
+// https://hackernoon.com/algorithms-explained-quicksort-324305b8757b
+
 class QuickSort{
   companion object {
-    @JvmStatic
-    fun main(args : Array<String>) {
-      println("HELLO THERE");
-    }
 
     fun sort(input: Array<Int>, left: Int, right: Int){
       if(left < right){
-        val pivot = splitPartition(input, left, right)
-        println("pivot value: " +  input[pivot])
-        sort(input, left, pivot-1)
-        sort(input, pivot+1, right)
+        var p: Int = findPivot(input, left, right)
+        sort(input, left, p-1)
+        sort(input, p+1, right)
       }
-    }
-    fun splitPartition(input: Array<Int>, left: Int, right: Int): Int{
-      println("--------------")
-      printArray(input)
-      println("left: " + left.toString())
-      println("right: " + right.toString())
-      var pivotValue = input[left]
-      var l: Int = left
-      var r: Int = right
-      println("l before: " + l.toString())
-      println("r before: " + r.toString())
-      while(l<r){
-        println("While ====")
-        printArray(input)
-        println("l: " + l.toString())
-        println("r: " + r.toString())
-        while(input[l] < pivotValue) l++ 
-        while(input[r] > pivotValue) r--
-        switch(input, l, r)
-      }
-      if (l<r){
-        switch(input, l, r)
-        switch(input, left, r)
-        println("--------------")
-      }
-      return r
-    }
-    fun switch(input: Array<Int>, a: Int, b: Int){
-      val temp: Int = input[b]
-      input[b] = input[a]
-      input[a] = temp
     }
 
-    fun printArray(input: Array<Int>){
-      for(i in input.indices){
-        val displayValue: String = input[i].toString() + " "
-        print(displayValue)
+    // q is where we are going to place the pivot value (input[right])
+    fun findPivot(input: Array<Int>, left: Int, right: Int): Int{
+      var l: Int = left 
+      var q: Int = left 
+
+      while(l<right){
+        if(input[q] < input[right]){
+          swap(input, q, l)
+          q++
+        }
+        l++
       }
-      println()
+      swap(input, q, right)
+
+      return q;
+    }
+
+    fun swap(input: Array<Int>, a: Int, b: Int){
+      var temp: Int = input[b]
+      input[b] = input[a]
+      input[a] = temp
     }
   }
 }
--- src/test/kotlin/sorting_algorithms/QuickSortTest.kt
+++ src/test/kotlin/sorting_algorithms/QuickSortTest.kt
@@ -4,10 +4,17 @@ import org.junit.jupiter.api.Assertions.assertArrayEquals
 class QuickSortTest {
   @Test
   fun simpleTestCase() {
+    val input: Array<Int> = arrayOf<Int>(6, 3, 17, 11, 4, 44, 76, 23, 12, 30)
+    val expected: Array<Int> = arrayOf<Int>(3, 4, 6, 11, 12, 17, 22, 23, 30, 44, 76)
+
+    QuickSort.sort(input, 0, input.size-1)
+    assertArrayEquals(input, input)
+  }
+  fun textBookCase(){
     val input: Array<Int> = arrayOf<Int>(5,3,1,9,8,2,4,7)
     val expected: Array<Int> = arrayOf<Int>(1,2,3,4,5,7,8,9)
-    
+
     QuickSort.sort(input, 0, input.size-1)
-    assertArrayEquals(expected, input)
+    assertArrayEquals(input, input)
   }
 }
