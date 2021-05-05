fc526787b2f05cc397f90312fe78e206b9835e7f
Michael Lennox Kent <lennoxkent@gmail.com>
2021-05-05 07:25:05 +0200

    add heap structures

---
+++ src/main/kotlin/sorting_algorithms/Heap.kt
@@ -0,0 +1,15 @@
+import kotlin.math.floor
+import kotlin.Double
+
+class Heap{
+  companion object {
+    fun constructHeapBottomUp(input: Array<Int>){
+      val lastParentIndex: Int = floor(input.size/(2.toDouble())).toInt()
+
+      
+      
+
+
+    }
+  }
+}
---
+++ src/test/kotlin/sorting_algorithms/HeapTest.kt
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+
+class HeapTest {
+  @Test
+  fun simpleTestCase() {
+    val input: Array<Int> = arrayOf<Int>(1,10,4,7,2,4)
+    val expected: Array<Int> = arrayOf<Int>(10,5,7,4,2,1)
+
+    Heap.constructHeapBottomUp(input)
+    // assertArrayEquals(expected, input)
+  }
+}
