50d033fedb56c87f6cbc206a02226419e7c46de3
Michael Lennox Kent <lennoxkent@gmail.com>
2022-05-27 07:37:08 +0200

    add comments and clean up

--- src/main/kotlin/sorting_algorithms/Heap.kt
+++ src/main/kotlin/sorting_algorithms/Heap.kt
@@ -4,17 +4,20 @@ import kotlin.Double
 class Heap{
   companion object {
     fun constructHeapBottomUp(input: Array<Int>){
-      // using n determine if the input array could be a valid heap
+      // Using n determine if the input array could be a valid heap
       val lastParentIndex: Int = floor(input.size/(2.toDouble())).toInt() -1 // -1: index from 0
       val last: Int = input.size - 1
      
+      // Check if each of the parents is a heap.
+      // Starting from the last parent to the root,ie bottom-up approach.
       for(p in lastParentIndex downTo 0 ){
         var currentParent: Int = p
         val currentParentValue: Int = input[p]
         var isHeap: Boolean = false
-
+        // Check if each of the parents' children is a heap as well.
         while(!isHeap && 2*currentParent <= last){
           var currentChild: Int = 2*currentParent
+          // Determine which of it's children values to compare to the parent value.
           if(currentChild < last){
             if (input[currentChild] < input[(currentChild +1)]){
               currentChild = currentChild + 1
@@ -30,7 +33,6 @@ class Heap{
         }
         input[currentParent] = currentParentValue
       }
-      // for (element in input) { println(element) }
     }
   }
 }
