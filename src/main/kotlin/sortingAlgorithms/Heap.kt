755f5b34f61d3eb4c943fbdf33f8684639430d9e
Michael Lennox Kent <lennoxkent@gmail.com>
2022-10-18 08:16:13 +0200

    add basic working heap

--- src/main/kotlin/sortingAlgorithms/Heap.kt
+++ src/main/kotlin/sortingAlgorithms/Heap.kt
@@ -1,38 +1,39 @@
 import kotlin.math.floor
-import kotlin.Double
 
 class Heap{
   companion object {
-    fun constructHeapBottomUp(input: Array<Int>){
+    fun constructHeapBottomUp(input :Array<Int>){
       // Using n determine if the input array could be a valid heap
-      val lastParentIndex: Int = floor(input.size/(2.toDouble())).toInt() -1 // -1: index from 0
-      val last: Int = input.size - 1
-     
+      val n :Int = input.size
+      val lastParentIndex: Int = floor(n/(2.toDouble())).toInt() -1 // -1: index from 0
+
       // Check if each of the parents is a heap.
-      // Starting from the last parent to the root,ie bottom-up approach.
-      for(p in lastParentIndex downTo 0 ){
-        var currentParent: Int = p
-        val currentParentValue: Int = input[p]
-        var isHeap: Boolean = false
-        // Check if each of the parents' children is a heap as well.
-        while(!isHeap && 2*currentParent <= last){
-          var currentChild: Int = 2*currentParent
-          // Determine which of it's children values to compare to the parent value.
-          if(currentChild < last){
-            if (input[currentChild] < input[(currentChild +1)]){
-              currentChild = currentChild + 1
-            }
-            if(currentParentValue >= input[currentChild]){
-              isHeap = true
-            }
-            else{
-              input[currentParent] = input[currentChild]
-              currentParent = currentChild
-            }
+      // Starting from the last parent to the root, ie bottom-up approach.
+      for(i in lastParentIndex downTo 0 ){
+        var k :Int = i
+        val v :Int = input[k]
+        var isHeap :Boolean = false
+
+        while(isHeap.equals(false) && 2*k < n){
+          var j :Int = 2*k
+          if (j + 1 < n && input[j+1] > input[j])  j = j + 1
+          if (v > input[j]) {
+            isHeap = true
           }
+          else {
+            input[k] = input[j]
+            k = j
+          }
+          input[k] = v
         }
-        input[currentParent] = currentParentValue
       }
     }
+    fun swap(a :Int, b :Int, input :Array<Int>){
+      val temp :Int = input[a]
+      input[a] = input[b]
+      input[b] = temp
+    }
+
   }
 }
+
